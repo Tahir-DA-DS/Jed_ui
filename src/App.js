7c5ef1d19a96ef1531f3486421c11259b7455470
Tahir-DA-Ds <tahir.adeleye@gmail.com>
2024-05-29 11:07:22 +0100

    footer fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import RegistrationForm from './components/registration'
-import Detailspage from './components/details'
-import Dialog from './components/dialog'
-import './App.css'
-import Header from './components/navsection'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RegistrationForm from './components/registration';
+import Detailspage from './components/details';
+import Dialog from './components/dialog';
+import './App.css';
+import Header from './components/navsection';
 import CreateRRR from './components/RRRgen';
+import Footer from './components/footer';
 
 function App() {
-  const [showDialog, setShowDialog] = useState(false)
+  const [showDialog, setShowDialog] = useState(false);
 
   useEffect(() => {
     // Check if it's the user's first visit
@@ -22,19 +23,21 @@ function App() {
   const handleCloseDialog = () => {
     setShowDialog(false);
   };
+
   return (
     <Router>
-
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={ <RegistrationForm />}/>
-        <Route path="/details" element={ <Detailspage />}/>
-        <Route path="/gen-invoice" element={<CreateRRR />} />
-      </Routes>
-     
-    </div>
-    {showDialog && <Dialog onClose={handleCloseDialog} />}
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<RegistrationForm />} />
+            <Route path="/details" element={<Detailspage />} />
+            <Route path="/gen-invoice" element={<CreateRRR />} />
+          </Routes>
+        </main>
+        <Footer />
+        {showDialog && <Dialog onClose={handleCloseDialog} />}
+      </div>
     </Router>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>© 2024 Skyrun Electric Power Technology. All rights reserved.</p>
+
+        <div className="footer-links">
+        <a href="https://skyrunelectric-ng.onrender.com/store.html">Product</a>
+          <a href="https://skyrunelectric-ng.onrender.com/About.html">About</a>
+          <a href="https://skyrunelectric-ng.onrender.com/Contact.html">Contact Us</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer
